d33ab276c13af1aa5fd1d3278182cb1585f8b231
Stümpfl, Maximilian <maximilian.stuempfl@exxeta.com>
2023-05-20 21:12:47 +0200

    start wallet page

---
+++ src/app/(home)/wallet/(components)/sidebar-nav.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+
+import { cn } from '@/lib/classnames';
+import { buttonVariants } from '@/components/ui/button';
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+] as const;
+
+interface SidebarNavProps extends React.HTMLAttributes<HTMLElement> {}
+
+export function SidebarNav({ className, ...props }: SidebarNavProps) {
+  const searchParams = useSearchParams();
+  const currentMonth = searchParams.get('month') ?? months[new Date().getMonth()];
+  // TODO only show if searcparams contains "year"
+  // than calculate the current month and set searchparam
+  // https://nextjs.org/docs/app/api-reference/functions/use-search-params#updating-searchparams
+
+  return (
+    <nav className={cn('flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1', className)} {...props}>
+      {searchParams.get('year') ? (
+        <>
+          {months.map((item) => {
+            return (
+              <Link
+                key={item}
+                href={{
+                  pathname: '/wallet',
+                  query: { month: item },
+                }}
+                className={cn(
+                  buttonVariants({ variant: 'ghost' }),
+                  currentMonth === item
+                    ? 'bg-muted-foreground text-white hover:bg-muted-foreground hover:text-white'
+                    : 'hover:bg-muted-foreground hover:text-white ',
+                  'justify-start'
+                )}
+              >
+                {item}
+              </Link>
+            );
+          })}
+        </>
+      ) : (
+        <div className='text-muted-foreground'>Select a year</div>
+      )}
+    </nav>
+  );
+}
--- src/app/(home)/wallet/layout.tsx
+++ src/app/(home)/wallet/layout.tsx
@@ -2,8 +2,8 @@ import { Suspense } from 'react';
 import { Metadata } from 'next';
 
 import { Separator } from '@/components/ui/separator';
-import { homeConfig } from '@/app/(home)/constants';
-import { SidebarNav } from '@/app/(home)/settings/(components)/sidebar-nav';
+import { SidebarNav } from '@/app/(home)/wallet/(components)/sidebar-nav';
+import { YearSwitcher } from '@/app/(home)/wallet/(components)/year-switcher';
 
 export const metadata: Metadata = {
   title: 'Forms',
@@ -14,19 +14,20 @@ interface SettingsLayoutProps {
   children: React.ReactNode;
 }
 
-export default function SettingsLayout({ children }: SettingsLayoutProps) {
+export default function WalletLayout({ children }: SettingsLayoutProps) {
   return (
     <>
       <div className='hidden space-y-6 p-10 pb-16 md:block'>
         <div className='space-y-0.5'>
-          <h2 className='text-2xl font-bold tracking-tight'>Settings</h2>
-          <p className='text-muted-foreground'>Manage your account settings and set e-mail preferences.</p>
+          <h2 className='text-2xl font-bold tracking-tight'>Wallet</h2>
+          <p className='text-muted-foreground'>Manage your expenses for each month.</p>
         </div>
         <Separator className='my-6' />
         <div className='flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0'>
-          <aside className='-mx-4 lg:w-1/5'>
+          <aside className='-mx-4 space-y-6 lg:w-1/5'>
             <Suspense fallback={<Fallback />}>
-              <SidebarNav items={homeConfig.sidebarNav} />
+              <YearSwitcher />
+              <SidebarNav />
             </Suspense>
           </aside>
           <div className='flex-1 lg:max-w-2xl'>{children}</div>
