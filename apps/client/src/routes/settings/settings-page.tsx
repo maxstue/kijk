beae28f627fd0bbf436c56a69281faf78eeb5ee4
maxstue <maxstue2304@gmail.com>
2023-09-06 09:50:38 +0200

    client: better nav lists in settings

--- apps/client/src/routes/settings/settings-page.tsx
+++ apps/client/src/routes/settings/settings-page.tsx
@@ -8,38 +8,36 @@ import { cn } from '@/lib/utils';
 
 export function SettingsPage() {
   return (
-    <>
-      <div className='space-y-6 p-10 pb-16'>
-        <div className='space-y-0.5'>
-          <h2 className='text-2xl font-bold tracking-tight'>Settings</h2>
-          <p className='text-muted-foreground'>Manage your account settings and set e-mail preferences.</p>
-        </div>
-        <Separator className='my-6' />
-        <div className='flex flex-col space-y-8 lg:flex-row lg:space-y-0'>
-          <aside className=' lg:w-1/5'>
-            <nav className={cn('flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1')}>
-              {settingsNav.map(({ to, label, icon }) => {
-                const Icon = Icons[icon || 'arrowRight'];
-                return (
-                  <Link
-                    key={label}
-                    to={'/settings/$section'}
-                    params={{ section: to }}
-                    activeProps={{ className: 'bg-accent-foreground text-white' }}
-                    className={cn(buttonVariants({ variant: 'ghost' }), 'justify-start')}
-                  >
-                    <Icon className='mr-2 h-4 w-4' />
-                    {label}
-                  </Link>
-                );
-              })}
-            </nav>
-          </aside>
-          <div className='w-full flex-1 lg:pl-12'>
-            <Outlet />
-          </div>
+    <div className='space-y-6 p-10 pb-16'>
+      <div className='space-y-0.5'>
+        <h2 className='text-2xl font-bold tracking-tight'>Settings</h2>
+        <p className='text-muted-foreground'>Manage your account settings and set e-mail preferences.</p>
+      </div>
+      <Separator className='my-6' />
+      <div className='flex flex-col space-y-8 lg:flex-row lg:space-y-0'>
+        <aside className=' lg:w-1/5'>
+          <nav className={cn('flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1')}>
+            {settingsNav.map(({ to, label, icon }) => {
+              const Icon = Icons[icon || 'arrowRight'];
+              return (
+                <Link
+                  key={label}
+                  to={'/settings/$section'}
+                  params={{ section: to }}
+                  activeProps={{ className: 'bg-accent-foreground text-accent' }}
+                  className={cn(buttonVariants({ variant: 'ghost' }), 'justify-start text-accent-foreground ')}
+                >
+                  <Icon className='mr-2 h-4 w-4' />
+                  {label}
+                </Link>
+              );
+            })}
+          </nav>
+        </aside>
+        <div className='w-full flex-1 lg:pl-12'>
+          <Outlet />
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- apps/client/src/routes/settings/settings-section-route.tsx
+++ apps/client/src/routes/settings/settings-section-route.tsx
@@ -1,14 +1,22 @@
 import { lazyRouteComponent, Route } from '@tanstack/react-router';
 import * as z from 'zod';
 
+import { ErrorSimple } from '@/components/error-simple';
+import { settingsNav } from '@/lib/constants';
 import { settingsRoute } from '@/routes/settings/settings-route';
 
-// TODO make param typesafe only allow a certain string
+const availableparams = settingsNav.map((x) => x.to) as string[];
+
 export const settingsSectionRoute = new Route({
   getParentRoute: () => settingsRoute,
   path: '$section',
   parseParams: (params) => ({
-    section: z.string().parse(params.section),
+    section: z
+      .string()
+      .refine((arg) => availableparams.includes(arg))
+      .parse(params.section),
   }),
+  // TODO show notfound error
+  errorComponent: ({ error }) => <ErrorSimple error={error as Error} />,
   component: lazyRouteComponent(() => import('./settings-section-page'), 'SettingsSectionPage'),
 });
