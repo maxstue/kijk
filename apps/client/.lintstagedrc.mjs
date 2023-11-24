9591bc6ce7ade0b4f14888ce629fec36f7189c63
maxstue <maxstue2304@gmail.com>
2023-11-24 23:32:05 +0100

    KIJK-179: remove pre-commit hooks

--- apps/client/.lintstagedrc.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-  // Lint and Prettify TS and JS files
-  'src/**/*.{js,ts,jsx,tsx}': ['prettier --write -u --ignore-path=.gitignore'],
-  // Prettify only Markdown and JSON files
-  '{docs,src}/**/*.{json,css,scss,md}': ['prettier -w --ignore-path=.gitignore'],
-};
