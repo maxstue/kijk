16df534e149eeeab2d29e2da2eac5591079a3d1a
maxstue <maxstue2304@gmail.com>
2024-01-07 19:10:48 +0100

    fix(client): remove router devtools in prod

--- apps/client/src/routes/root-page.tsx
+++ apps/client/src/routes/root-page.tsx
@@ -1,13 +1,25 @@
+import React from 'react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export default function RootPage() {
   return (
     <>
       <Outlet />
-      <TanStackRouterDevtools position='bottom-left' />
-      <ReactQueryDevtools buttonPosition='bottom-right' />
+      <TanStackRouterDevtools initialIsOpen={false} position='bottom-left' />
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-right' />
     </>
   );
 }
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
