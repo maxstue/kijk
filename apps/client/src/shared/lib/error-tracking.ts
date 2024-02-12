0a0e152fab436029d6a7a6e777166350c241208b
maxstue <maxstue2304@gmail.com>
2024-02-12 07:53:55 +0100

    feat(client): improve sentry

--- apps/client/src/shared/lib/error-tracking.ts
+++ apps/client/src/shared/lib/error-tracking.ts
@@ -6,16 +6,27 @@ export const createSentry = () =>
   Sentry.init({
     dsn: env.SentryDsn,
     environment: env.Mode,
-    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay(), new Sentry.BrowserProfilingIntegration()],
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+      Sentry.browserProfilingIntegration(),
+    ],
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
-
     // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
     tracePropagationTargets: ['localhost', /^https:\/\/kijk-api.fly\.dev\/api/, /^https:\/\/kijk-client.vercel\.app/],
+
     // Capture Replay for 10% of all sessions,
     // plus for 100% of sessions with an error
     replaysSessionSampleRate: 0.1,
     replaysOnErrorSampleRate: 1.0,
+
+    // Set profilesSampleRate to 1.0 to profile every transaction.
+    // Since profilesSampleRate is relative to tracesSampleRate,
+    // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
+    // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
+    // results in 25% of transactions being profiled (0.5*0.5=0.25)
+    profilesSampleRate: 1.0,
   });
