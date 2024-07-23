b062b461704c28232c3fed4cdbc84676826290ff
maxstue <maxstue2304@gmail.com>
2024-07-23 10:47:11 +0200

    fix mail

--- apps/web/constants/config.ts
+++ apps/web/constants/config.ts
@@ -3,7 +3,7 @@ import { Route } from 'next/types';
 export const siteConfig = {
   name: 'kijk',
   url: 'https://kijk-maxstue.vercel.app/',
-  email: 'maxstue2304@gmailc.com',
+  email: 'mail:maxstue2304@gmail.com',
   docs_user: '/docs/user',
   docs_dev: '/docs/developer',
   description: 'Beautifully designed Budget book built with shadcn/ui and nextjs',
