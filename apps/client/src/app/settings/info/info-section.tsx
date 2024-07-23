d73bea7e709e42a243f795c40322964934c16eba
maxstue <maxstue2304@gmail.com>
2024-07-23 11:06:49 +0200

    feat(KIJK-134): add legal pages links to client

--- apps/client/src/app/settings/info/info-section.tsx
+++ apps/client/src/app/settings/info/info-section.tsx
@@ -1,5 +1,10 @@
+import { ExternalLink } from 'lucide-react';
+
 import { AppVersion } from '@/shared/components/app-version';
+import { buttonVariants } from '@/shared/components/ui/button';
 import { Separator } from '@/shared/components/ui/separator';
+import { env } from '@/shared/env';
+import { cn } from '@/shared/lib/helpers';
 
 export function InfoSection() {
   return (
@@ -9,9 +14,31 @@ export function InfoSection() {
         <p className='text-sm text-muted-foreground'>Here are some informations about this app.</p>
       </div>
       <Separator />
-      <div className='flex items-center gap-4'>
-        <div>Version: </div>
-        <AppVersion className='text-muted-foreground' />
+      <div className='flex flex-col gap-12'>
+        <div className='flex items-center gap-4'>
+          <div>Version: </div>
+          <AppVersion className='text-muted-foreground' />
+        </div>
+        <div className='flex gap-4'>
+          <a
+            href={`${env.WebUrl}/terms`}
+            rel='noopener noreferrer'
+            target='_blank'
+            className={cn(buttonVariants({ variant: 'ghost' }), 'group gap-2')}
+          >
+            Terms of service
+            <ExternalLink className='h-4 w-4' />
+          </a>
+          <a
+            href={`${env.WebUrl}/privacy`}
+            rel='noopener noreferrer'
+            target='_blank'
+            className={cn(buttonVariants({ variant: 'ghost' }), 'group gap-2')}
+          >
+            Privacy Policy
+            <ExternalLink className='h-4 w-4' />
+          </a>
+        </div>
       </div>
     </div>
   );
