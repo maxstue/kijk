8eda3f2c1ff6006754fd866fa3f4fec5b51a8d32
maxstue <dev@justmax.xyz>
2025-05-30 18:52:30 +0200

    fix: dbcontext design time creation

--- apps/api/src/Infrastructure/Persistence/AppDbContext.cs
+++ apps/api/src/Infrastructure/Persistence/AppDbContext.cs
@@ -1,10 +1,20 @@
 ﻿using Kijk.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 namespace Kijk.Infrastructure.Persistence;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+public class AppDbContext : DbContext
 {
+    public AppDbContext()
+    {
+
+    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
     public DbSet<Household> Households => Set<Household>();
     public DbSet<UserHousehold> UserHouseholds => Set<UserHousehold>();
     public DbSet<Consumption> Consumptions => Set<Consumption>();
@@ -14,6 +24,17 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
 
     public DbSet<Role> Roles => Set<Role>();
     public DbSet<Permission> Permissions => Set<Permission>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // This is only needed to be able to run the efbundle script
+        if (!optionsBuilder.IsConfigured)
+        {
+            // This is a placeholder string because it will be replaced with the action one via the cli.
+            optionsBuilder.UseNpgsql("DefaultConnection");
+            optionsBuilder.ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning));
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
