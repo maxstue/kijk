b23574058e9670304d3d77032c5d86caef19d06f
maxstue <dev@justmax.xyz>
2026-06-01 23:29:51 +0200

    refactor(client): decouple error tracking from router

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { router } from '@/router';
 import { AnalyticsService } from '@/shared/lib/analytics-client';
 import { ErrorService } from '@/shared/lib/error-tracking';
 import { welcome } from '@/shared/utils/string';
@@ -10,7 +11,7 @@ import { App } from './app';
 import '@kijk/ui/globals.css';
 
 welcome();
-ErrorService.init();
+ErrorService.init(router);
 AnalyticsService.init();
 
 ReactDOM.createRoot(document.querySelector('#app')!).render(
